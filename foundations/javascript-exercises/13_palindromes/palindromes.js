792bea6c354284ffcb3985de6c35e0da3eab43b4
Calcifer Chang <142045045+calciferchang@users.noreply.github.com>
2025-07-26 18:10:00 -0400

    finished palindromes

--- foundations/javascript-exercises/13_palindromes/palindromes.js
+++ foundations/javascript-exercises/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+  let cleanedPalindrome = palindrome.replace(/[^\w\s]|_/g, "").toLowerCase().replace(/\s+/g, '')
+  let isPalindrome = cleanedPalindrome.split('').reverse().join('')
+  if (cleanedPalindrome.toLowerCase() === isPalindrome) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
