83c85eb68ff83778ef46bf6cd96861271f40c547
Kiki Chen <142045045+calciferchang@users.noreply.github.com>
2025-07-27 14:59:49 -0400

    found the boomers

--- foundations/javascript-exercises/16_findTheOldest/findTheOldest.js
+++ foundations/javascript-exercises/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const ages = people.map(
+    (person) =>
+      (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth
+  );
+  const oldest = ages.indexOf(Math.max(...ages));
+  return people[oldest];
 };
 
 // Do not edit below this line
