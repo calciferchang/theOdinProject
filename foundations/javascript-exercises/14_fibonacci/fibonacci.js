e0fb271684f5ce9d03757082e51f49226bbc124f
Kiki Chen <142045045+calciferchang@users.noreply.github.com>
2025-07-26 21:59:25 -0400

    finished fibonacci

--- foundations/javascript-exercises/14_fibonacci/fibonacci.js
+++ foundations/javascript-exercises/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (x) {
+  let fibIndex = parseInt(x);
+  if (fibIndex < 0) {
+    return "OOPS";
+  }
+  let fibArray = [0, 1];
+  for (let i = 0; i < fibIndex - 1; i++) {
+    fibArray.push(
+      fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]
+    );
+  }
+  return fibArray[fibIndex];
 };
 
 // Do not edit below this line
